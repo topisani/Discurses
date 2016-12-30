e4aba27181af10624a6f0c405403988618e0f355
topisani <tobiaspisani@gmail.com>
2016-12-30 01:02:11 +0100

    Fix bug with dateline on every new message

--- discurses/ui/message_list.py
+++ discurses/ui/message_list.py
@@ -39,7 +39,7 @@ class MessageListWidget(urwid.WidgetWrap):
         self.discord.ui.draw_screen()
 
     def _on_message(self, message):
-        if message.timestamp.date() != self.list_walker[0].message.timestamp.date():
+        if message.timestamp.date() != self.list_walker[-1].message.timestamp.date():
             self.list_walker.append(DatelineWidget(self.chat_widget, message.timestamp.date()))
         if message.channel in self.chat_widget.channels:
             self.add_message(message)
