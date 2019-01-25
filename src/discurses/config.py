69aca4e5f278cb8f46f1da844465251f1d7bcf8d
snus-kin <tcarroll@unclelee.ch>
2019-01-25 19:56:16 +0000

    implement better notification (basic for MacOS for now), change to subprocess rather than os.system calls)

--- src/discurses/config.py
+++ src/discurses/config.py
@@ -1,11 +1,15 @@
-import yaml
 import os
 import shlex
+import platform
+import subprocess
+import yaml
 
 CONFIG_FILE_PATH = os.path.join(
     os.path.expanduser("~"), ".config", "discurses.yaml")
 CACHE_DIR_PATH = os.path.join(os.path.expanduser("~"), ".cache", "discurses")
 CACHE_AVATARS_PATH = os.path.join(CACHE_DIR_PATH, "avatars")
+
+PLATFORM = platform.system()
 
 try:
     with open(CONFIG_FILE_PATH, 'r') as file:
@@ -23,9 +27,23 @@ def create_dir(directory):
         os.makedirs(directory)
     return directory
 
-
 create_dir(CACHE_AVATARS_PATH)
 
+def macos_notify(title, text):
+    """
+    Create a MacOS notification
+    Snippet from:
+    https://stackoverflow.com/questions/17651017/python-post-osx-notification/41318195#41318195
+    """
+    os.system("""
+              osascript -e 'display notification "{}" with title "{}"'
+              """.format(text, title))
+
+def linux_notify(arguments):
+    """
+    Create a linux notification
+    """
+    subprocess.Popen(["notify-send", arguments])
 
 async def send_notification(discord, message):
     """
@@ -35,23 +53,27 @@ async def send_notification(discord, message):
     avatar = await discord.get_avatar(message.author)
     nickname = message.author.display_name
     if message.channel.is_private:
-        os.system(
-            "notify-send -i {avatar} \"{author} in chat with {users}:\" {content}"
-            .format(
-                avatar=shlex.quote(avatar),
-                author=shlex.quote(nickname),
-                content=shlex.quote(message.clean_content),
-                users=shlex.quote(', '.join(a.display_name for a in message.channel.recipients))))
+        if PLATFORM == "Linux" and table['notify']:
+            linux_notify("-i {avatar} \"{author} in chat with {users}:\" {content}"
+                         .format(
+                             avatar=shlex.quote(avatar),
+                             author=shlex.quote(nickname),
+                             content=shlex.quote(message.clean_content),
+                             users=shlex.quote(', '
+                                               .join(a.display_name for a in message.channel.recipients))))
+        elif PLATFORM == "Darwin" and table['notify']:
+            macos_notify(shlex.quote(nickname), shlex.quote(message.clean_content))
     else:
-        os.system(
-            "notify-send -i {avatar} \"{author} in {server}#{channel}\" {content}"
-            .format(
-                avatar=shlex.quote(avatar),
-                author=shlex.quote(nickname),
-                server=shlex.quote(message.server.name),
-                channel=shlex.quote(message.channel.name),
-                content=shlex.quote(message.clean_content)))
-
+        if PLATFORM == "Linux" and table['notify']:
+                linux_notify("-i {avatar} \"{author} in {server}#{channel}\" {content}"
+                              .format(
+                                  avatar=shlex.quote(avatar),
+                                  author=shlex.quote(nickname),
+                                  server=shlex.quote(message.server.name),
+                                  channel=shlex.quote(message.channel.name),
+                                  content=shlex.quote(message.clean_content)))
+        elif PLATFORM == "Darwin" and table['notify']:
+            macos_notify(shlex.quote(nickname), shlex.quote(message.clean_content))
 
 def file_picker(callback, chat_widget):
     """
